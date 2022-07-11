6d972ffdecef7ac78c1d6c3ac680410c5f86898a
Mina  Philemon <mina.fayek@outlook.com>
2022-07-11 15:30:58 +0200

    Added typescript and Vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App() {
+
+  return (
+    <div className='app'>
+     Hello, beoble
+    </div>
+  )
+}
+
+export default App
